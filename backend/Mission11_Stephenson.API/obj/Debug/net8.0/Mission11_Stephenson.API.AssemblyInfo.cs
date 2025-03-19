7d30808b93cdb844f3e6bbe9935e8ec63c4ee5cd
ccstephenson <ccstephenson@gmail.com>
2025-03-19 16:25:23 -0600

    some changes

--- backend/Mission11_Stephenson.API/obj/Debug/net8.0/Mission11_Stephenson.API.AssemblyInfo.cs
+++ backend/Mission11_Stephenson.API/obj/Debug/net8.0/Mission11_Stephenson.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Stephenson.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f055fa4f9c541855ab5d505a7c54bc137c1ae5be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72e985f725d05688905ff340403824be87746e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Stephenson.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Stephenson.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
