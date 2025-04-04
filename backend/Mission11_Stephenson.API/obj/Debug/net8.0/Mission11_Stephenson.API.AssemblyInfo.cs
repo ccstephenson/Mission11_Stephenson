6850e85caf26ceaf0a87b9df00be73a49063b202
ccstephenson <ccstephenson@gmail.com>
2025-04-04 17:13:11 -0600

    Starting to deploy. Already deployed the backend and got the backend URL into the API file

--- backend/Mission11_Stephenson.API/obj/Debug/net8.0/Mission11_Stephenson.API.AssemblyInfo.cs
+++ backend/Mission11_Stephenson.API/obj/Debug/net8.0/Mission11_Stephenson.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Stephenson.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b01740e628ab6e7479e67f4e7554c5f375ec3dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd47d358d97eb8985f295e572eea807855aae167")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Stephenson.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Stephenson.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
