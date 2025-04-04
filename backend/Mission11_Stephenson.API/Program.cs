a73d391023e44257dc4e000b43782c41950d0777
ccstephenson <ccstephenson@gmail.com>
2025-04-04 17:27:25 -0600

    Changes to the withorigins for cors errors

--- backend/Mission11_Stephenson.API/Program.cs
+++ backend/Mission11_Stephenson.API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddDbContext<BookstoreContext>(options =>
 builder.Services.AddCors(options => 
     options.AddPolicy("AllowBookstore",
     policy => {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("https://jolly-mud-0606f931e.6.azurestaticapps.net")
             .AllowAnyMethod()
             .AllowAnyHeader();
     })
--- backend/Mission11_Stephenson.API/obj/Debug/net8.0/Mission11_Stephenson.API.AssemblyInfo.cs
+++ backend/Mission11_Stephenson.API/obj/Debug/net8.0/Mission11_Stephenson.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Stephenson.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd47d358d97eb8985f295e572eea807855aae167")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e32c6ce851f235fb725357b3fc5ed9a2a3266f0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Stephenson.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Stephenson.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
